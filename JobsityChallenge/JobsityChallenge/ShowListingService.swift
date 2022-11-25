063a1fa3c30c42f8bc50a0022ae052612184c6c0
joao pedro <amorim_tst@profusion.mobi>
2022-11-25 20:25:46 -0300

    feat(Service): create ShowListingService

---
+++ JobsityChallenge/JobsityChallenge/ShowListingService.swift
@@ -0,0 +1,28 @@
+//
+//  ShowListingService.swift
+//  JobsityChallenge
+//
+//  Created by João Pedro de Amorim on 25/11/22.
+//
+
+import Foundation
+
+struct ShowListingService {
+    enum ShowListingServiceError: Error {
+        case invalidURL
+    }
+
+    func getShows() async throws -> [ShowModel] {
+        // TODO: Implement pagination
+        let endpointSuffix = "shows?page=0"
+
+        guard let url = URL(string: Environment.baseEndpoint + endpointSuffix) else {
+            throw ShowListingServiceError.invalidURL
+        }
+
+        let (data, _) = try await URLSession.shared.data(from: url)
+
+        let shows = try JSONDecoder().decode([ShowModel].self, from: data)
+        return shows
+    }
+}
